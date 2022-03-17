9cec8973a68f1f27f2856cc4ba87c7ad3bf44624
aguustin <94425896+aguustin@users.noreply.github.com>
2022-03-17 19:48:00 -0300

    Clase de instalacion y configuracion de entorno

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Tienda agustin.
+        Tienda de ecommerce Mole
         </p>
         <a
           className="App-link"
