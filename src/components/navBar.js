76cfeb929cfafcfd9f04fe916bccbd302ca39c4e
aguustin <94425896+aguustin@users.noreply.github.com>
2022-03-24 21:22:52 -0300

    clase 2

---
+++ src/components/navBar.js
@@ -0,0 +1,20 @@
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Elogo from '../ecommerceLogo.png';
+
+const navBar = () => {
+    return (
+        <nav className="navbar col-md-14 col-lg-14 col-sm-14">
+            <img src={Elogo} className="nav-img" alt=""></img>
+            <div className="mx-auto">
+            <button className="nav-Buttons">Computers</button>
+            <button className="nav-Buttons">Phones</button>
+            <button className="nav-Buttons">Watches</button>
+            </div>
+            <button className="B-login">Login</button>
+        </nav>
+
+    );
+}
+
+export default navBar;
